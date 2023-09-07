ea65fb0f5c453465fe3880b2623f5b54c845aca7
Mpaulariveros9 <mpaulariveros9@gmail.com>
2023-09-06 21:27:37 -0500

    ajustes

--- src/main/java/com/backend/digitalhouse/clinicaodontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
+++ src/main/java/com/backend/digitalhouse/clinicaodontologica/dto/entrada/odontologo/OdontologoEntradaDto.java
@@ -1,7 +1,6 @@
 package com.backend.digitalhouse.clinicaodontologica.dto.entrada.odontologo;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
--- src/main/java/com/backend/digitalhouse/clinicaodontologica/dto/entrada/turno/TurnoEntradaDto.java
+++ src/main/java/com/backend/digitalhouse/clinicaodontologica/dto/entrada/turno/TurnoEntradaDto.java
@@ -2,7 +2,6 @@ package com.backend.digitalhouse.clinicaodontologica.dto.entrada.turno;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
